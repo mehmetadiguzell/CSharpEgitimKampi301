61293578bec9f857711947527835c6a53fafafe2
Mehmet Adıgüzel <adiguzelmehmet1997@gmail.com>
2025-03-19 10:16:11 +0300

    18.Bölüm Sonu

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        KampContext db = new KampContext();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = db.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            var deletedEntity = db.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            db.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            var result = _object.ToList();
+            return result;
+        }
+
+        public T GetById(int id)
+        {
+            var result = _object.Find(id);
+            return result;
+        }
+
+        public void Insert(T entity)
+        {
+            db.Entry(entity).State = EntityState.Added;
+            db.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
